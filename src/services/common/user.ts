766a8df9337023bbaf2329fc0aee248a0acec2ae
Dario Chamorro <dario.chamorro.v@gmail.com>
2017-10-30 16:28:10 -0500

    Test on user service implemented

--- src/services/common/user.ts
+++ src/services/common/user.ts
@@ -1,5 +1,5 @@
 export class User{
-    _id:string;
+    _id?:string;
     nombre:string;
     email:string;
     celular:string;
--- src/services/db-connection.ts
+++ src/services/db-connection.ts
@@ -1,25 +1,27 @@
 import { config } from '../config/global';
 import { MongoClient, Db } from 'mongodb';
 
-class DBConnection {
+export class DBConnection {
 
     db: Db;
+    
+    constructor(configDB:any, callback:()=>void = null) {
 
-    constructor() {
-
-        const connection = config.database.host
-            + ":" + config.database.port
-            + "/" + config.database.database;
+        const connection = configDB.host
+            + ":" + configDB.port
+            + "/" + configDB.database;
 
         MongoClient.connect(connection)
             .then(db => {
                 this.db = db;
                 db.collection("restaurants")
                     .createIndex({localizacion:"2dsphere"});
+                    
+                if(callback) callback();
             })
             .catch(err => console.log(err))
     }
 
 }
 
-export const Con = new DBConnection();
+export const Con = new DBConnection(config.database, ()=>{});
